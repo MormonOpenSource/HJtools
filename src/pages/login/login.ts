2ef792ab1d734552583928719a484f2065fd7c5a
Fabian  Monsalve <fabtics@gmail.com>
2018-03-04 23:51:51 -0500

    adding loading and alert in login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { TabsPage } from '../tabs/tabs';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth'
 import { UserLogin } from '../../interfaces/user-login'
 import { RegisterPage } from '../register/register';
@@ -19,8 +19,13 @@ import { RegisterPage } from '../register/register';
 export class LoginPage {
 
   user = {} as UserLogin;
+  loading;
 
-  constructor(private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private fire: AngularFireAuth,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
@@ -30,16 +35,36 @@ export class LoginPage {
     }
   }
 
-  async login(user: UserLogin) {
-    try {
-      const result = await this.fire.auth.signInWithEmailAndPassword(user.email, user.password);
-      if (result) {
-        this.navCtrl.setRoot(TabsPage);
-      }
-    }
-    catch (e) {
-      console.error(e);
-    }
+  createLoader(message: string = "Por favor espera...") { // Optional Parameter
+    this.loading = this.loadingCtrl.create({
+      content: message
+    });
+  }
+
+  presentAlert(message) {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: message,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  login(user: UserLogin) {
+    this.createLoader();
+    this.loading.present().then(() => {
+      this.fire.auth.signInWithEmailAndPassword(user.email, user.password)
+        .then((result) => {
+          this.loading.dismiss();
+          if (result) {
+            this.navCtrl.setRoot(TabsPage);
+          }
+        })
+        .catch(e => {
+          this.presentAlert(e.message);
+          this.loading.dismiss();
+        })
+    })
   }
 
   register() {
