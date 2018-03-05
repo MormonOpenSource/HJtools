d8a59488932bc00913eb474fd355e68b52654f58
Fabian  Monsalve <fabtics@gmail.com>
2018-03-05 10:54:23 -0500

    assign permissions page

--- src/pages/permissions/permissions.ts
+++ src/pages/permissions/permissions.ts
@@ -20,6 +20,7 @@ export class PermissionsPage {
   private permForm: FormGroup;
   private usersQuery: AngularFireList<any>;
   private users: Observable<any[]>;
+  private userSelected: any;
 
   constructor(private db: AngularFireDatabase, 
     public navCtrl: NavController, 
@@ -27,17 +28,36 @@ export class PermissionsPage {
     private formBuilder: FormBuilder) {
 
       this.usersQuery = db.list('/users');
-      this.users = this.usersQuery.valueChanges()
+      this.users = this.usersQuery.snapshotChanges().map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      })
       this.permForm = this.formBuilder.group({
         uid: ['', Validators.required],
-        role: ['', Validators.required]
+        role: ['', Validators.required],
+        stake: ['', Validators.required],
+        ward: ['', Validators.required]
       });
   }
 
   ionViewDidLoad() {}
+
+  onChangeUser(uid) {
+    this.users.forEach((users:any) =>{
+      users.forEach(user => {
+        if(user.uid === uid) {
+          this.userSelected = user;
+          this.permForm.controls.role.setValue(user.role);
+          this.permForm.controls.stake.setValue(user.stake)
+          this.permForm.controls.ward.setValue(user.ward)
+          return;
+        }
+      });
+    })
+  }
   
   changeRole() {
-
+    this.usersQuery.update(this.userSelected.key, this.permForm.value)
+    this.navCtrl.pop();
   }
 
 }
